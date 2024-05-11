41a4804f6fb0a55cad531f6ed5711d2cbd5ba9bc
KimJeaCheol <cresh0105@gmila.com>
2024-05-11 10:52:47 +0900

    spring3.2 + gradle + java17 초기 설정

---
+++ src/test/java/com/iron/sprignsample/SprignsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iron.sprignsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprignsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
